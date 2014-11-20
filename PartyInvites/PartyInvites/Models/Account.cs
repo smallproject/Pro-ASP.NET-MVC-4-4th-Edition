6dabc76469c902e062f65bc3847b53e9e49e6117
smallproject <j.a.villas@live.com.ph>
2014-11-20 14:48:58 +0100

    End of chapter 2

---
+++ PartyInvites/PartyInvites/Models/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class Account
+    {
+        [Display(Name = "First name")]
+        public string firstname { get; set; }
+
+        [Display(Name = "Middle name")]
+        public string middlename { get; set; }
+
+        [Display(Name = "Last name")]
+        public string lastname { get; set; }
+
+        [Display(Name = "Gender")]
+        public string gender { get; set; }
+
+        [Display(Name = "Birthdate")]
+        public DateTime birthdate { get; set; }
+
+        [Display(Name = "E-mail")]
+        public string email { get; set; }
+
+        [Display(Name = "check box to agree with conditions")]
+        public bool agree { get; set; }
+    }
+}
---
+++ PartyInvites/PartyInvites/Models/Register.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace PartyInvites.Models
+{
+    public class Register
+    {
+        [Display(Name = "Username: ")]
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+        
+        [Display(Name = "Password: ")]
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+        
+        [Display(Name = "First name: ")]
+        [Required(ErrorMessage = "First name is required.")]
+        public string Firstname { get; set; }
+
+        [Display(Name = "Last name: ")]
+        [Required(ErrorMessage = "Last name is required.")]
+        public string Lastname { get; set; }
+
+        [Display(Name = "Gender: ")]
+        [Required(ErrorMessage = "Gender is required.")]
+        public string gender { get; set; }
+
+        [Display(Name = "Birthdate:")]
+        [Required(ErrorMessage = "Birthdate is required.")]
+        public DateTime birthdate { get; set; }
+    }
+}
